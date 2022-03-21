787e3e5011f913feb28ef1f8a13a558d1fe1e721
joselinb <joselinb2001@gmail.com>
2022-03-21 15:33:54 -0400

    .

--- src/main/java/edu/wpi/team_u/Main.java
+++ src/main/java/edu/wpi/team_u/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Harsh Patel");
     System.out.println("Timothy Klein");
+    System.out.println("Joselin Barbosa");
     App.launch(App.class, args);
   }
 }
