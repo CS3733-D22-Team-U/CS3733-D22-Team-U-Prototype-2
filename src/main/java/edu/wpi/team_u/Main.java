a050ade2694507c76aeb63e3eb0fcd5f4bb71eea
Marko <mvila0119@gmail.com>
2022-03-21 15:39:38 -0400

    name

--- src/main/java/edu/wpi/team_u/Main.java
+++ src/main/java/edu/wpi/team_u/Main.java
@@ -7,6 +7,7 @@ public class Main {
     System.out.println("Timothy Klein");
     System.out.println("Joselin Barbosa");
     System.out.println("Michael Akstin");
+    System.out.println("Marko Vila");
     App.launch(App.class, args);
   }
 }
