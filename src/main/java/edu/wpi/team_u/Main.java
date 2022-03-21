407405a93107d42732ec89b8036bf93859acd245
krob <kodyrobinson42>
2022-03-21 15:41:44 -0400

    adding print statement with my name

--- src/main/java/edu/wpi/team_u/Main.java
+++ src/main/java/edu/wpi/team_u/Main.java
@@ -6,6 +6,7 @@ public class Main {
     System.out.println("Harsh Patel");
     System.out.println("Timothy Klein");
     System.out.println("Joselin Barbosa");
+    System.out.println("Kody Robinson");
     System.out.println("Michael Akstin");
     System.out.println("Marko Vila");
     App.launch(App.class, args);
