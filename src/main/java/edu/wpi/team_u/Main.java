b9b6cd789df5a69fe5e142df355adf3f72697aab
Deepti1314 <deepti.gosukonda13@gmail.com>
2022-03-21 15:38:57 -0400

    Deepti's name

--- src/main/java/edu/wpi/team_u/Main.java
+++ src/main/java/edu/wpi/team_u/Main.java
@@ -6,6 +6,7 @@ public class Main {
     System.out.println("Harsh Patel");
     System.out.println("Timothy Klein");
     System.out.println("Joselin Barbosa");
+    System.out.println("Deepti Gosukonda");
     App.launch(App.class, args);
   }
 }
