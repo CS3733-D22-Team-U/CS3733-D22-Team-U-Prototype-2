48fb2c7adebcbc34925530ac6881ae9298a5dd59
kodyd <kdrobinson@wpi.edu>
2022-03-21 16:54:38 -0400

    added name print statement

--- src/main/java/edu/wpi/team_u/Main.java
+++ src/main/java/edu/wpi/team_u/Main.java
@@ -11,7 +11,7 @@ public class Main {
     System.out.println("William Doyle");
     System.out.println("Marko Vila");
     System.out.println("Nick Biliouris");
-    System.out.println("Kody Robinson")
+    System.out.println("Kody Robinson");
 
     App.launch(App.class, args);
   }
