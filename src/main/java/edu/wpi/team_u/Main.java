c9c7321ede6c32a0e53a2eb5a306181046c443c7
HarshPatelSchool <harshpatelschool2021@gmail.com>
2022-03-21 15:24:44 -0400

    Added Harsh Patel print

--- src/main/java/edu/wpi/team_u/Main.java
+++ src/main/java/edu/wpi/team_u/Main.java
@@ -1,8 +1,9 @@
-package edu.wpi.teamname;
+package edu.wpi.team_u;
 
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("Harsh Patel");
     App.launch(App.class, args);
   }
 }
