8f40da1696ed0a8d0aacc868215afd2f8cf38a63
mjaxe <makstin33@gmail.com>
2022-03-21 15:38:53 -0400

    putting in my name: Mike A

--- src/main/java/edu/wpi/team_u/Main.java
+++ src/main/java/edu/wpi/team_u/Main.java
@@ -6,6 +6,7 @@ public class Main {
     System.out.println("Harsh Patel");
     System.out.println("Timothy Klein");
     System.out.println("Joselin Barbosa");
+    System.out.println("Michael Akstin");
     App.launch(App.class, args);
   }
 }
