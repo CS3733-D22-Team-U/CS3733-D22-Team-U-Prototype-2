93e1b46e4e3b9a069f1c3cb0738a5f8b6af03ccf
Marko <mvila0119@gmail.com>
2022-03-23 15:45:27 -0400

    Fixed merge issu  Please enter the commit message for your changes. Lines starting

--- src/main/java/edu/wpi/team_u/controllers/MedicineDeliveryController.java
+++ src/main/java/edu/wpi/team_u/controllers/MedicineDeliveryController.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class MedicineDeliveryController {
+
+  public Button backButton;
 
   public void backToServicePage(ActionEvent actionEvent) throws IOException {
     Scene scene = Uapp.getScene("edu/wpi/team_u/views/app.fxml");
