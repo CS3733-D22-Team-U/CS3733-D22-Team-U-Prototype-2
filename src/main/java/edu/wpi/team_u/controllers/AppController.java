c65b10afe24bf62462082363197616e57a20b2cf
Marko <mvila0119@gmail.com>
2022-03-24 19:57:08 -0400

    iicon test 2

--- src/main/java/edu/wpi/team_u/controllers/AppController.java
+++ src/main/java/edu/wpi/team_u/controllers/AppController.java
@@ -1,32 +1,23 @@
 package edu.wpi.team_u.controllers;
 
 import edu.wpi.team_u.Uapp;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class AppController implements Initializable {
+public class AppController {
 
   public TextField nameField;
   public Label outputLabel;
   public Button laundryService;
   public Button equipmentDelivery;
 
-  private ImageView equipmentDeliveryIcon;
   @FXML
   void sayHello() {
     String name = nameField.getText();
@@ -72,12 +63,5 @@ public class AppController implements Initializable {
     Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
     appStage.setScene(scene);
     appStage.show();
-  }
-
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    File file = new File("edu/wpi/team_u/icons/doctoricon.png");
-    Image image = new Image(file.toURI().toString());
-    equipmentDeliveryIcon.setImage(image);
   }
 }
