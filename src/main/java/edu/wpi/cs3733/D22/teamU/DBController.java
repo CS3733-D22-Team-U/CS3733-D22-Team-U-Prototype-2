76c801fd0499d45e36d9f3437b27aa04d0c2eb40
Harsh Patel <harshpatelschool2021@gmail.com>
2022-03-30 14:56:07 -0400

    Refactoring

---
+++ src/main/java/edu/wpi/cs3733/D22/teamU/DBController.java
@@ -0,0 +1,58 @@
+package edu.wpi.cs3733.D22.teamU;
+
+import edu.wpi.cs3733.D22.teamU.BackEnd.Udb;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.sql.SQLException;
+
+public class DBController {
+
+  public static void main(String[] args) throws IOException, SQLException {
+    String username, password;
+    try {
+      username = args[0];
+      password = args[1];
+    } catch (Exception e) {
+      username = "admin";
+      password = "admin";
+    }
+
+    File folder = new File("csvTables");
+    folder.mkdir();
+    InputStream csvLocationFile =
+        Main.class
+            .getClassLoader()
+            .getResourceAsStream("edu/wpi/cs3733/D22/teamU/csvTables/TowerLocations.csv");
+    String location = copyFile(csvLocationFile, "csvTables/TowerLocations.csv");
+    InputStream csvEmployee =
+        Main.class
+            .getClassLoader()
+            .getResourceAsStream("edu/wpi/cs3733/D22/teamU/csvTables/TowerEmployees.csv");
+    String employee = copyFile(csvEmployee, "csvTables/TowerEmployees.csv");
+    InputStream csvEquipment =
+        Main.class
+            .getClassLoader()
+            .getResourceAsStream("edu/wpi/cs3733/D22/teamU/csvTables/TowerEquipment.csv");
+    String equipment = copyFile(csvEquipment, "csvTables/TowerEquipment.csv");
+
+    String[] CSVfiles = {location, employee, equipment};
+
+    Udb udb = new Udb(username, password, CSVfiles);
+
+    // udb.menu(CSVfiles); //Uncomment this to start terminal menu
+  }
+
+  public static String copyFile(InputStream inputPath, String outputPath) throws IOException {
+    File f = new File(outputPath);
+    // f.createNewFile();
+    try {
+      Files.copy(inputPath, f.getAbsoluteFile().toPath());
+    } catch (Exception e) {
+      //Doesn't override if files already exist
+    }
+    inputPath.close();
+    return outputPath;
+  }
+}
---
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/EquipmentDeliverySystemController.java
@@ -0,0 +1,97 @@
+package edu.wpi.cs3733.D22.teamU.frontEnd.controllers;
+
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
+import edu.wpi.cs3733.D22.teamU.frontEnd.services.IService;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class EquipmentDeliverySystemController implements Initializable, IService {
+
+  @FXML JFXHamburger hamburger;
+  @FXML VBox vBoxPane;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    HamburgerBasicCloseTransition closeTransition = new HamburgerBasicCloseTransition(hamburger);
+    closeTransition.setRate(-1);
+    hamburger.addEventHandler(
+        MouseEvent.MOUSE_CLICKED,
+        e -> {
+          closeTransition.setRate(closeTransition.getRate() * -1);
+          closeTransition.play();
+          vBoxPane.setVisible(!vBoxPane.isVisible());
+        });
+  }
+
+  @Override
+  public void addRequest() {}
+
+  @Override
+  public void removeRequest() {}
+
+  @Override
+  public void updateRequest() {}
+
+  @Override
+  public void displayRequest() {}
+
+  public void toHome(ActionEvent actionEvent) throws IOException {
+    Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/HomePage.fxml");
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    appStage.setScene(scene);
+    appStage.show();
+  }
+
+  public void toLabRequest(ActionEvent actionEvent) throws IOException {
+    Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/labRequestServices.fxml");
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    appStage.setScene(scene);
+    appStage.show();
+  }
+
+  public void toMealDelivery(ActionEvent actionEvent) throws IOException {
+    Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/mealDelivery.fxml");
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    appStage.setScene(scene);
+    appStage.show();
+  }
+
+  public void toGiftAndFloral(ActionEvent actionEvent) throws IOException {
+    Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/giftFloralService.fxml");
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    appStage.setScene(scene);
+    appStage.show();
+  }
+
+  public void toLaundry(ActionEvent actionEvent) throws IOException {
+    Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/laundryService.fxml");
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    appStage.setScene(scene);
+    appStage.show();
+  }
+
+  public void toMedicineDelivery(ActionEvent actionEvent) throws IOException {
+    Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/medicineDelivery.fxml");
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    appStage.setScene(scene);
+    appStage.show();
+  }
+
+  public void toMap(ActionEvent actionEvent) throws IOException {
+    Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/map.fxml");
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    appStage.setScene(scene);
+    appStage.show();
+  }
+}
