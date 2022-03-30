568541786edb06f123ceedcad357645cc327bd38
Harsh Patel <harshpatelschool2021@gmail.com>
2022-03-30 13:40:46 -0400

    Create new csv folder

--- src/main/java/edu/wpi/team_u/BackEnd/Udb.java
+++ src/main/java/edu/wpi/team_u/BackEnd/Udb.java
@@ -1,9 +1,11 @@
 package edu.wpi.team_u.BackEnd;
 
 /**
- * ask about Harsh's override idea to simplify all the functions the tower locations master CSV does
- * NOT have unique nodes ask about changing all of our array lists to hash maps
+ * ask about Harsh's override idea to simplify all the functions
+ * the tower locations master CSV does NOT have unique nodes
+ * ask about changing all of our array lists to hash maps
  */
+
 import edu.wpi.team_u.BackEnd.Employee.EmployeeDaoImpl;
 import edu.wpi.team_u.BackEnd.Equipment.EquipmentDaoImpl;
 import edu.wpi.team_u.BackEnd.Location.LocationDaoImpl;
@@ -24,6 +26,7 @@ public class Udb {
     Udb udb = new Udb();
     String username = args[0];
     String password = args[1];
+
     File folder = new File("csvTables");
     folder.mkdir();
     InputStream csvLocationFile =
@@ -56,11 +59,11 @@ public class Udb {
   }
 
   public void start(String username, String password, String[] CSVfiles)
-      throws IOException, SQLException {
+          throws IOException, SQLException {
     locationImpl.DB_LOC = locationImpl.DB_LOC + "user=" + username + ";password=" + password + ";";
     EmployeeImpl.DB_LOC = EmployeeImpl.DB_LOC + "user=" + username + ";password=" + password + ";";
     EquipmentImpl.DB_LOC =
-        EquipmentImpl.DB_LOC + "user=" + username + ";password=" + password + ";";
+            EquipmentImpl.DB_LOC + "user=" + username + ";password=" + password + ";";
 
     try {
       Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
@@ -70,7 +73,7 @@ public class Udb {
       System.out.println("File | Project Structure, Modules, Dependency tab");
       System.out.println("Add by clicking on the green plus icon on the right of the window");
       System.out.println(
-          "Select JARs or directories. Go to the folder where the database JAR is located");
+              "Select JARs or directories. Go to the folder where the database JAR is located");
       System.out.println("Click OK, now you can compile your program and run it.");
       e.printStackTrace();
       return;
@@ -83,17 +86,17 @@ public class Udb {
       Statement exampleStatement = connection.createStatement();
 
       exampleStatement.executeUpdate(
-          "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.connection.requireAuthentication',true)");
+              "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.connection.requireAuthentication',true)");
       exampleStatement.executeUpdate(
-          "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.database.sqlAuthorization', true)");
+              "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.database.sqlAuthorization', true)");
       exampleStatement.executeUpdate(
-          "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY("
-              + "'derby.authentication.provider', 'BUILTIN')");
+              "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY("
+                      + "'derby.authentication.provider', 'BUILTIN')");
       exampleStatement.executeUpdate(
-          "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.user.admin', 'admin')");
+              "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.user.admin', 'admin')");
       exampleStatement.executeUpdate(
-          "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY("
-              + "'derby.database.defaultConnectionMode', 'fullAccess')");
+              "CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY("
+                      + "'derby.database.defaultConnectionMode', 'fullAccess')");
       connection.close();
     } catch (Exception e) {
       System.out.println("Wrong Username/Password");
@@ -121,11 +124,11 @@ public class Udb {
     Scanner userInput = new Scanner(System.in);
 
     System.out.println(
-        "What database would you like to chose: \n"
-            + "1 - Locations\n"
-            + "2 - Employees\n"
-            + "3 - Equipment\n"
-            + "4 - Quit");
+            "What database would you like to chose: \n"
+                    + "1 - Locations\n"
+                    + "2 - Employees\n"
+                    + "3 - Equipment\n"
+                    + "4 - Quit");
 
     switch (userInput.nextInt()) {
       case 1:
@@ -147,12 +150,12 @@ public class Udb {
     Scanner locationsInput = new Scanner(System.in);
 
     System.out.println(
-        "1 - List Location Information\n"
-            + "2 - Change Location Floor and Type\n"
-            + "3 - Enter New Location\n"
-            + "4 - Delete Location \n"
-            + "5 - Save Location Information to CSV file\n"
-            + "6 - Return to Main Menu");
+            "1 - List Location Information\n"
+                    + "2 - Change Location Floor and Type\n"
+                    + "3 - Enter New Location\n"
+                    + "4 - Delete Location \n"
+                    + "5 - Save Location Information to CSV file\n"
+                    + "6 - Return to Main Menu");
 
     switch (locationsInput.nextInt()) {
       case 1:
@@ -189,12 +192,12 @@ public class Udb {
     Scanner employeeInput = new Scanner(System.in);
 
     System.out.println(
-        "1 - List Employee Information\n"
-            + "2 - Edit Employee Information\n"
-            + "3 - Enter New Employee\n"
-            + "4 - Delete Employee\n"
-            + "5 - Save Employee Information to CSV\n"
-            + "6 - Return to Main Menu");
+            "1 - List Employee Information\n"
+                    + "2 - Edit Employee Information\n"
+                    + "3 - Enter New Employee\n"
+                    + "4 - Delete Employee\n"
+                    + "5 - Save Employee Information to CSV\n"
+                    + "6 - Return to Main Menu");
     switch (employeeInput.nextInt()) {
       case 1:
         EmployeeImpl.printEmployeeTableInTerm(CSVfiles[1]);
@@ -227,12 +230,12 @@ public class Udb {
     Scanner equipmentInput = new Scanner(System.in);
 
     System.out.println(
-        "1 - List Equipment Information\n"
-            + "2 - Edit Equipment Information\n"
-            + "3 - Enter New Equipment\n"
-            + "4 - Delete Equipment\n"
-            + "5 - Save Equipment Information to CSV\n"
-            + "6 - Return to Main Menu");
+            "1 - List Equipment Information\n"
+                    + "2 - Edit Equipment Information\n"
+                    + "3 - Enter New Equipment\n"
+                    + "4 - Delete Equipment\n"
+                    + "5 - Save Equipment Information to CSV\n"
+                    + "6 - Return to Main Menu");
     switch (equipmentInput.nextInt()) {
       case 1:
         EquipmentImpl.printEquipTableInTerm(CSVfiles[2]);
@@ -260,4 +263,5 @@ public class Udb {
         break;
     }
   }
+
 }
