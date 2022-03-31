11727c82bc10d56f2a921be63c3ecd207bed7d21
willdoyle757 <61399895+willdoyle757@users.noreply.github.com>
2022-03-30 21:07:24 -0400

    Employee and equpment Addition

--- src/main/java/edu/wpi/cs3733/D22/teamU/Testing.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/Testing.java
@@ -1,15 +1,24 @@
-package edu.wpi.cs3733.D22.teamU.BackEnd.Test;
+package edu.wpi.cs3733.D22.teamU;
 
 import edu.wpi.cs3733.D22.teamU.BackEnd.Location.LocationDaoImpl;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 public class Testing {
+
+  String locTestCSV;
+
+  public Testing(String[] CSVfiles) {
+    this.locTestCSV = CSVfiles[4];
+  }
 
   public String DB_LOC = "jdbc:derby:UDB;";
   public LocationDaoImpl locationImpl = new LocationDaoImpl(DB_LOC);
 
   @Test
-  public void locCSVToJavaTest() {}
+  public void locCSVToJavaTest() throws IOException {
+    locationImpl.CSVToJava(locTestCSV);
+  }
 
   @Test
   public void locJavaToCSVTest() {}
