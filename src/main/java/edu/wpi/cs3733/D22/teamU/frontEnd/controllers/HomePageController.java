ba97123f9e9683b9a6254b00f40c400335fba96c
krob <kodyrobinson42>
2022-04-03 20:36:54 -0400

    dots correctly showed for 2 panes earlier but "Map" button no longer opens anything

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/HomePageController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/HomePageController.java
@@ -1,9 +1,15 @@
 package edu.wpi.cs3733.D22.teamU.frontEnd.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HomePageController extends ServiceController {
 
@@ -18,6 +24,7 @@ public class HomePageController extends ServiceController {
 
   @Override
   public void updateRequest() {}
+
 
   public void toCloseApp(ActionEvent actionEvent) {
     Platform.exit();
