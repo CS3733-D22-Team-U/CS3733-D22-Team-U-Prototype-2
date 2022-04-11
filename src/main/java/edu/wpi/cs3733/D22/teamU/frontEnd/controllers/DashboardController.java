b4f2a9a8de383b968fbba69bbaba0e16ba5ebd44
Marko Vila <mvila0119@gmail.com>
2022-04-11 00:09:29 -0400

    Added buttons to nav tab and fixed animatiuon

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/DashboardController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/DashboardController.java
@@ -5,9 +5,13 @@ import java.util.ResourceBundle;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public class DashboardController extends ServiceController {
@@ -20,10 +24,20 @@ public class DashboardController extends ServiceController {
   @FXML AnchorPane clockPane;
   @FXML ImageView clockPaneArrow;
 
+  @FXML AnchorPane anchor;
+
+  @FXML ButtonBar topRow;
+  @FXML ButtonBar bottomRow;
+
+  @FXML Pane backgroundPane;
+  private static final String HOVERED_BUTTON = "-fx-border-color: #029ca6";
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     handleNavPaneAnimation();
     handleClockPaneAnimation();
+
+    handleNavButtons();
   }
 
   private void handleNavPaneAnimation() {
@@ -38,19 +52,49 @@ public class DashboardController extends ServiceController {
           } else {
             closeNav.setToX(846);
             closeNav.play();
+            TranslateTransition closeOther = new TranslateTransition(new Duration(350), clockPane);
+            closeOther.setToX(0);
+            closeOther.play();
           }
         });
   }
 
+  private void handleNavButtons() {
+    for (Node node : topRow.getButtons()) {
+      Button button = (Button) node;
+      String initStyle = button.getStyle();
+      button.setStyle(initStyle);
+      button.setOnMouseEntered(e -> button.setStyle(initStyle + HOVERED_BUTTON));
+      button.setOnMouseExited(e -> button.setStyle(initStyle));
+    }
+    for (Node node : bottomRow.getButtons()) {
+      Button button = (Button) node;
+      String initStyle = button.getStyle();
+      button.setStyle(initStyle);
+      button.setOnMouseEntered(e -> button.setStyle(initStyle + HOVERED_BUTTON));
+      button.setOnMouseExited(e -> button.setStyle(initStyle));
+    }
+  }
+
   private void handleClockPaneAnimation() {
     TranslateTransition openNav = new TranslateTransition(new Duration(350), clockPane);
+    openNav.setToX(-846);
     TranslateTransition closeNav = new TranslateTransition(new Duration(350), clockPane);
-    openNav.setToX(-936);
 
     clockButton.setOnAction(
         (ActionEvent evt) -> {
           clockPaneArrow.setRotate(clockPaneArrow.getRotate() * -1);
-          openNav.play();
+          if (clockPane.getTranslateX() != -846) {
+            openNav.play();
+            TranslateTransition closeOther =
+                new TranslateTransition(new Duration(350), animatePane);
+            closeOther.setToX(0);
+            closeOther.play();
+            navPaneArrow.setRotate(navPaneArrow.getRotate() * -1);
+          } else {
+            closeNav.setToX(0);
+            closeNav.play();
+          }
         });
   }
 
@@ -70,4 +114,22 @@ public class DashboardController extends ServiceController {
 
   @Override
   public void updateRequest() {}
+
+  public void closeNavTabs(MouseEvent mouseEvent) {
+    if (mouseEvent.getY() < animatePane.getLayoutY()
+        || mouseEvent.getY() > animatePane.getLayoutY() + animatePane.getHeight()) {
+      if (animatePane.getTranslateX() != 0) {
+        TranslateTransition closeNav = new TranslateTransition(new Duration(350), animatePane);
+        closeNav.setToX(0);
+        closeNav.play();
+        navPaneArrow.setRotate(navPaneArrow.getRotate() * -1);
+      }
+      if (clockPane.getTranslateX() == -846) {
+        TranslateTransition closeNav = new TranslateTransition(new Duration(350), clockPane);
+        closeNav.setToX(0);
+        closeNav.play();
+        clockPaneArrow.setRotate(navPaneArrow.getRotate() * -1);
+      }
+    }
+  }
 }
